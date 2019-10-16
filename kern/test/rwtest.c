9e3c18a68a3f3a57c0ea05d5f829cc67c171dc1c
Eigenstir <trinity@zion>
2019-10-16 15:17:39 +0000

    revisiting rwtest

--- kern/test/rwtest.c
+++ kern/test/rwtest.c
@@ -30,10 +30,11 @@ static volatile unsigned long testval3;
 static struct semaphore *donesem;
 static struct rwlock *test_rw = NULL;
 
-static
-bool
-failif(bool condition){
-  if (condition){
+static bool
+failif(bool condition)
+{
+  if (condition)
+  {
     spinlock_acquire(&status_lock);
     test_status = TEST161_FAIL;
     spinlock_release(&status_lock);
@@ -41,62 +42,64 @@ failif(bool condition){
   return condition;
 }
 
-static 
-void
-readtestthread(void *junk1, unsigned long num){
+static void
+readtestthread(void *junk1, unsigned long num)
+{
   //acquire the read lock and check that shared critical section was not mutated unexpectedly
-  //along the way. 
-	(void)junk1;
-	(void)num;
+  //along the way.
+  (void)junk1;
+  (void)num;
 
   unsigned i;
   random_yielder(4);
 
-	rwlock_acquire_read(test_rw);
-  for (i=0; i < NLOOPS; i++){
+  rwlock_acquire_read(test_rw);
+  for (i = 0; i < NLOOPS; i++)
+  {
     kprintf_t(".");
-    
+
     failif(testval2 != testval1 * testval1);
     random_yielder(4);
 
-    failif(testval2%3 != (testval3*testval3)%3);
+    failif(testval2 % 3 != (testval3 * testval3) % 3);
     random_yielder(4);
 
-    failif(testval3 != testval1%3);
+    failif(testval3 != testval1 % 3);
     random_yielder(4);
   }
 
-	rwlock_release_read(test_rw);
+  rwlock_release_read(test_rw);
   V(donesem);
-	return;
+  return;
 }
 
-static 
-void
-writetestthread(void *junk, unsigned long num){
+static void
+writetestthread(void *junk, unsigned long num)
+{
   //mutate testval1 as a shared critical section and yield the CPU. check that the value
   //is what was expected - ie. was not mutated outside of this function.
-	(void)junk;
+  (void)junk;
+  (void)num;
 
   unsigned i;
-  kprintf_t("%d", num);
   random_yielder(4);
 
-	rwlock_acquire_write(test_rw);
-  for (i=0; i < NLOOPS; i++){
+  rwlock_acquire_write(test_rw);
+  for (i = 0; i < NLOOPS; i++)
+  {
     kprintf_t(".");
     testval1 = num;
-    testval2 = num*num;
-    testval3 = num%3;
+    testval2 = num * num;
+    testval3 = num % 3;
 
     random_yielder(4);
     failif(test_rw->readers_in != 0);
 
-    testval1 = num*num;
+    testval1 = num * num;
     random_yielder(4);
     failif(testval1 != testval2);
 
-    testval1 = num%3;
+    testval1 = num % 3;
     random_yielder(4);
     failif(testval1 != testval3);
 
@@ -107,114 +110,126 @@ writetestthread(void *junk, unsigned long num){
     random_yielder(4);
     failif(test_rw->readers_in != 0);
   }
-	rwlock_release_write(test_rw);
+  rwlock_release_write(test_rw);
   kprintf_t("writer out: %u\n", num);
   V(donesem);
 }
 
-int 
-rwtest(int nargs, char **args) 
+int rwtest(int nargs, char **args)
 {
-	(void)nargs;
-	(void)args;
+  (void)nargs;
+  (void)args;
 
-	int i, result;
+  int i, result;
   unsigned long num = 9;
 
   testval1 = num;
-  testval2 = num*num;
-  testval3 = num%3;
-  
+  testval2 = num * num;
+  testval3 = num % 3;
+
   kprintf_n("Starting rwt1...\n");
-  for (i=0; i<CREATELOOPS; i++){
+  for (i = 0; i < CREATELOOPS; i++)
+  {
     kprintf_t(".");
     test_rw = rwlock_create("test_rw"); //try to create reader-writer lock
-    if (test_rw==NULL){
+    if (test_rw == NULL)
+    {
       panic("rwt1: rwlock_create failed\n");
     }
 
     donesem = sem_create("donesem", 0); //try to create a done semaphore
-    if (donesem == NULL){
+    if (donesem == NULL)
+    {
       panic("rwt1: sem_create failed\n");
     }
 
-    if (i != CREATELOOPS-1){ //clean up we error out early
+    if (i != CREATELOOPS - 1)
+    { //clean up we error out early
       rwlock_destroy(test_rw);
       sem_destroy(donesem);
     }
   }
 
-  spinlock_init(&status_lock); 
+  spinlock_init(&status_lock);
   test_status = TEST161_SUCCESS;
 
-  for (i=0; i<NREADERTHREADS; i++){
+  for (i = 0; i < NREADERTHREADS; i++)
+  {
     kprintf_t(".");
     result = thread_fork("reader", NULL, readtestthread, NULL, i); //test readers
-    if (result){
+    if (result)
+    {
       panic("rwt1: thread_fork failed: %s\n", strerror(result));
     }
   }
 
-  for (i=0; i<NWRITERTHREADS; i++){
+  for (i = 0; i < NWRITERTHREADS; i++)
+  {
     kprintf_t(".");
     result = thread_fork("reader", NULL, writetestthread, NULL, i); //test writer
-    if (result){
+    if (result)
+    {
       panic("rwt1: thread_fork failed: %s\n", strerror(result));
     }
   }
 
-  for (i=0; i<(NREADERTHREADS+NWRITERTHREADS);i++){
+  for (i = 0; i < (NREADERTHREADS + NWRITERTHREADS); i++)
+  {
     kprintf_t(".");
     P(donesem);
   }
 
   rwlock_destroy(test_rw);
-  test_rw= NULL;
+  test_rw = NULL;
   sem_destroy(donesem);
-  donesem=NULL;
+  donesem = NULL;
 
   kprintf_t("\n");
   success(test_status, SECRET, "rwt1");
 
-	return 0;
+  return 0;
 }
 
-int rwtest2(int nargs, char **args) {
-	(void)nargs;
-	(void)args;
+int rwtest2(int nargs, char **args)
+{
+  (void)nargs;
+  (void)args;
 
-	kprintf_n("rwt2 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt2");
+  kprintf_n("rwt2 unimplemented\n");
+  success(TEST161_FAIL, SECRET, "rwt2");
 
-	return 0;
+  return 0;
 }
 
-int rwtest3(int nargs, char **args) {
-	(void)nargs;
-	(void)args;
+int rwtest3(int nargs, char **args)
+{
+  (void)nargs;
+  (void)args;
 
-	kprintf_n("rwt3 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt3");
+  kprintf_n("rwt3 unimplemented\n");
+  success(TEST161_FAIL, SECRET, "rwt3");
 
-	return 0;
+  return 0;
 }
 
-int rwtest4(int nargs, char **args) {
-	(void)nargs;
-	(void)args;
+int rwtest4(int nargs, char **args)
+{
+  (void)nargs;
+  (void)args;
 
-	kprintf_n("rwt4 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt4");
+  kprintf_n("rwt4 unimplemented\n");
+  success(TEST161_FAIL, SECRET, "rwt4");
 
-	return 0;
+  return 0;
 }
 
-int rwtest5(int nargs, char **args) {
-	(void)nargs;
-	(void)args;
+int rwtest5(int nargs, char **args)
+{
+  (void)nargs;
+  (void)args;
 
-	kprintf_n("rwt5 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt5");
+  kprintf_n("rwt5 unimplemented\n");
+  success(TEST161_FAIL, SECRET, "rwt5");
 
-	return 0;
+  return 0;
 }
